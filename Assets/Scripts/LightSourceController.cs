99eb363020f32c024cdc48bd9eb4e176ee73b371
David <dan57@case.edu>
2018-09-30 13:03:48 -0400

    added basic sanity and power functionality

---
+++ Assets/Scripts/LightSourceController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourceController : MonoBehaviour {
+	private GameObject Player;
+	private PlayerCharacter playersScript;
+	bool atLight;
+	GameObject pointLight;
+
+
+	void Start () {
+		Player = GameObject.Find ("Player");
+		playersScript = (PlayerCharacter) Player.GetComponent(typeof(PlayerCharacter));
+		pointLight = new GameObject("The Light");
+		Light lightComp = pointLight.AddComponent<Light>();
+		lightComp.intensity = 5;
+		pointLight.transform.position = gameObject.transform.position;
+		pointLight.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (atLight) {
+			if (Input.GetKey (KeyCode.E)){
+				pointLight.SetActive (true);
+				playersScript.reducePower ();
+				playersScript.gainSanity ();
+			}
+		}
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (other.name == "Player")
+			atLight = true;
+	}
+
+	void OnTriggerExit(Collider other) {
+		atLight = false;
+	}
+
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,24 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCharacter : MonoBehaviour {
 	private GUIStyle style1 = new GUIStyle();
     private Inventory inventory;
+	private const int MAX_POWER = 300;
+	private int power;
+	private const int MAX_SANITY = 1000;
+	private int sanity;
+	private bool losingPower; 
+	private bool losingSanity;
 
     void Start () {
 		style1.fontSize = 25;
 		style1.normal.textColor = Color.green;
         inventory = GetComponent<Inventory>();
+		power = MAX_POWER; 
+		sanity = MAX_SANITY;
+		losingPower = false;
+		losingSanity = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+		sanity--;
+		if (sanity < 0) {
+			endGame ();
+		}
 	}
 
 	void OnGUI() {
-		GUI.Label (new Rect (Screen.width - 160, 20, 200, 200), ("Batteries: " + inventory.itemCount()), style1);
+		GUI.Label (new Rect (Screen.width - 160, 0, 200, 200), ("Batteries: " + inventory.itemCount()), style1);
+		GUI.Label (new Rect (Screen.width - 160, 20, 200, 200), ("Power: " + power / (MAX_POWER/100)), style1);
+		GUI.Label (new Rect (Screen.width - 160, 40, 200, 200), ("Sanity: " + sanity / (MAX_SANITY/100)), style1);	
 	}
 
     //test for picking up batteries and use them for Test Scene One
@@ -32,4 +48,33 @@ public class PlayerCharacter : MonoBehaviour {
             }
         }
     }
+
+	public int getPower(){
+		return this.power;
+	}
+
+	public void reducePower(){
+		this.power--;
+		if(power < 0){
+			endGame ();
+		}
+	}
+
+	public void gainSanity(){
+		if (this.sanity < MAX_SANITY - 3) {
+			this.sanity += 5;
+		} else {
+			this.sanity = MAX_SANITY;
+		}
+
+	}
+
+
+	public void stopReducingPower(){
+		losingPower = false;
+	}
+
+	public void endGame(){
+		SceneManager.LoadScene (0);
+	}
 }

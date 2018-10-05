90b49f8eb4bd86eac67fb875ba6b2bdd679b3f53
Rongjie Zeng <fatcha14@gmail.com>
2018-10-05 17:41:44 -0400

    InventoryUI

---
+++ Assets/Scripts/SanityBarUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    Image sanityBar;
+    float maxSanity = 100f;
+    public static float sanity;
+
+	// Use this for initialization
+	void Start () {
+        sanityBar = GetComponent<Image>();
+        sanity = maxSanity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sanityBar.fillAmount = sanity / maxSanity;
+	}
+}

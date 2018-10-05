ae4f4a5d152a442397963bd2f2df1d06e380b57e
Rongjie Zeng <fatcha14@gmail.com>
2018-10-05 18:58:33 -0400

    Sanity/Power Bar Added

--- Assets/Scripts/SanityBarUI.cs
+++ Assets/Scripts/SanityBarUI.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SanityBarUI : MonoBehaviour {
 
-    Image sanityBar;
-    float maxSanity = 100f;
-    public static float sanity;
+    [SerializeField] private Image powerBar;
+    [SerializeField]private Image sanityBar;
+    private float maxSanity = 100f;
+    private float maxPower = 0f;
+    private float sanity;
+    [SerializeField] private PlayerCharacter playerChar;
+    private float power;
 
-	// Use this for initialization
-	void Start () {
-        sanityBar = GetComponent<Image>();
-        sanity = maxSanity;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        sanityBar.fillAmount = sanity / maxSanity;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        playerChar = GameObject.FindWithTag("Player").GetComponent<PlayerCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maxPower == 0f)
+        {
+            maxPower = playerChar.getPowerSource().getMaxPower();
+        }
+        else
+        {
+            power = playerChar.getPower();
+            sanity = playerChar.getSanity();
+            sanityBar.fillAmount = sanity / maxSanity;
+            powerBar.fillAmount = power / maxPower;
+        }
+    }
+
 }

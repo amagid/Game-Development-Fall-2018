285afa82df2f6b9f4b8b7f782433c6874100fed2
Aaron Magid <ahm64@case.edu>
2018-10-07 14:39:28 -0400

    feat(InteractablePower): Added Interactability script to define how we can interact with an object.

---
+++ Assets/Scripts/Interactability.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactability : MonoBehaviour {
+    public bool canBePowered;
+    public bool canSiphonPower;
+    public bool isItem;
+    public bool isNote;
+}

3fbd7ccf6977ff76ab3228006e9133af57b6109b
melodyli <melodyli9237@gmail.com>
2018-10-28 12:09:01 -0400

    Battery inventory

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -7,36 +7,32 @@ public class InventoryUI : MonoBehaviour
 {
     private Inventory playerInventory;
     [SerializeField] private GameObject item1UI;
-
-
+    List<Image> imgs;
+    private int numBatteries = 0;
 
 
     // Use this for initialization
     void Start()
     {
+        imgs = new List<Image>();
         playerInventory = GameObject.Find("Player").GetComponent<Inventory>();
+        for (int i = 0; i < 8; i++) {
+            int j = i + 1;
+            string tag = "Slot" + j;
+            imgs.Add(GameObject.Find(tag).GetComponent<Image>());
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (!playerInventory.hasBattery())
-        {
-            item1UI.SetActive(true);
+        numBatteries = playerInventory.batteryCount();
+        Debug.Log(numBatteries);
+        for (int i = 0; i < numBatteries; i++){
+            imgs[i].color = UnityEngine.Color.green;
         }
-        else
-        {
-            item1UI.SetActive(false);
-        }
-
-
-        item1UI.SetActive(playerInventory.containItem("Battery(1)"));
-        if (playerInventory.containItem("Battery(1)")) {
-            Debug.Log("abcde");
-        }
-        item1UI.SetActive(!item1UI.activeInHierarchy);
-
+  
 
     }
 }

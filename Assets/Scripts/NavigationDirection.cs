fb882888348d52a9c94e1b53689d84024c95fb30
Catherine Tsuei <cwt26@case.edu>
2018-11-02 18:40:01 -0400

    feat(compassDirectionDisplay):player direction displayed
    As the player moves, the direction the player is traveling in is displayed.

--- Assets/Scripts/NavigationDirection.cs
+++ Assets/Scripts/NavigationDirection.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class NavigationDirection : MonoBehaviour {
-	private GUIStyle left = new GUIStyle();
 	[SerializeField] private GameObject player;
 	[SerializeField] private Text directionText;
 	private enum CardinalDirection
 	{
 		North, South, East, West
-	}
-
-	// Use this for initialization
-	void Start () {
-
 	}
 	
 	// Update is called once per frame
@@ -22,24 +16,15 @@ public class NavigationDirection : MonoBehaviour {
 		var v = player.transform.forward;
 		v.y = 0;
 		v.Normalize ();
-		Debug.Log ("PAY ATTENTION ********");
-		Debug.Log (v);
-		Debug.Log (Vector3.forward);
-		Debug.Log (Vector3.Angle (v, Vector3.forward));
-		Debug.Log ("PAY ATTENTION ********");
 
 		if (Vector3.Angle (v, Vector3.forward) <= 45.0) {
 			directionText.text = CardinalDirection.North.ToString();
-			Debug.Log ("North");
 		} else if (Vector3.Angle (v, Vector3.right) <= 45.0) {
 			directionText.text = CardinalDirection.East.ToString();
-			Debug.Log ("East");
 		} else if (Vector3.Angle (v, Vector3.back) <= 45.0) {
 			directionText.text = CardinalDirection.South.ToString();
-			Debug.Log ("South");
 		} else {
 			directionText.text = CardinalDirection.West.ToString();
-			Debug.Log ("West");
 		}
 	}
 }

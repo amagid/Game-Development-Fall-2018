cb2abcdcfced68d7e9cb987aa79532dc312f2e87
David <dan57@case.edu>
2018-11-01 21:28:10 -0400

    chore(cleanup): Removed unnecessary code from CentralLightController.

--- Assets/Scripts/CentralLightController.cs
+++ Assets/Scripts/CentralLightController.cs
@@ -6,51 +6,19 @@ public class CentralLightController : MonoBehaviour, DirectOperation {
 
 	private Inventory inventory;
 	[SerializeField] private GameObject central_lights;
-	private GameObject GreenLights;
-	private GameObject RedLights;
 
 	private bool deviceActive = false;
 	private GameObject battery;
 
-	//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	void Start()
 	{
 		this.deviceActive = false;
 		inventory = GameObject.Find("Player").GetComponent<Inventory>();
-		Transform gl = this.gameObject.transform.Find("GreenLights");
-		Transform rl = this.gameObject.transform.Find("RedLights");
-		if (gl != null)
-		{
-			this.GreenLights = gl.gameObject;
-		}
-		if (rl != null)
-		{
-			this.RedLights = rl.gameObject;
-		}
 	}
-	//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-	// Update is called once per frame
-	void Update()
-	{
-	}
 
 	public void activate()
 	{
-		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		if (this.GreenLights != null && !this.GreenLights.activeInHierarchy)
-		{
-			this.GreenLights.SetActive(true);
-		}
-		if (this.RedLights != null && this.RedLights.activeInHierarchy)
-		{
-			this.RedLights.SetActive(false);
-		}
-		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 		central_lights.SetActive(true);
 	}
 
@@ -58,16 +26,7 @@ public class CentralLightController : MonoBehaviour, DirectOperation {
 
 	public void deactivate()
 	{
-		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		if (this.GreenLights != null && this.GreenLights.activeInHierarchy)
-		{
-			this.GreenLights.SetActive(false);
-		}
-		if (this.RedLights != null && !this.RedLights.activeInHierarchy)
-		{
-			this.RedLights.SetActive(true);
-		}
-		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 		this.battery = null;
 		central_lights.SetActive(false);
 		deviceActive = false;

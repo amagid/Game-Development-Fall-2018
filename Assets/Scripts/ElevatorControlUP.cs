dd11aa2f168428f639c761bb93f42d83ca40c6f4
TungHo Lin(Dennis) <alexanderdennis@live.hk>
2018-11-07 19:40:30 -0500

    <faet>up button now works.

--- Assets/Scripts/ElevatorControlUP.cs
+++ Assets/Scripts/ElevatorControlUP.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeDoorSwitch : MonoBehaviour {
+public class ElevatorControlUP : MonoBehaviour {
 
+    [SerializeField] private GameObject scene_controller;
+    [SerializeField] private GameObject button_light;
     private PowerConsumer powerConsumer;
-    [SerializeField] GameObject maze_controller;
+    private int current_level_num;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         this.powerConsumer = this.getPowerConsumer();
+        current_level_num = 1;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         bool deviceIsPowered = this.powerConsumer.powerDevice();
         if (deviceIsPowered)
         {
-            maze_controller.GetComponent<MazeController>().nextMazeSequence();
+            if(current_level_num < 3)
+            {
+                current_level_num++;
+                button_light.SetActive(true);
+                scene_controller.GetComponent<SceneController>().StartCoroutine("loadLevel", current_level_num);
+                button_light.SetActive(false);
+            }
         }
     }
 

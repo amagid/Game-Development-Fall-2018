c9af8b01cb193622272cfa0693410f5d28ae5d63
Rongjie Zeng <fatcha14@gmail.com>
2018-11-08 10:29:28 -0500

    Revert "Revert "Add the pause function to the tablet""
    This reverts commit b7ee3ba0947ae31e59b4d49916c5f6f8b285238e.

--- Assets/Scripts/TabletUI.cs
+++ Assets/Scripts/TabletUI.cs
@@ -6,6 +6,9 @@ public class TabletUI : MonoBehaviour {
         
     [SerializeField] private GameObject Tablet;
     [SerializeField] private GameObject inventoryBar;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject camera;
+
     private bool isOn;
     // Use this for initialization
     void Start()
@@ -36,13 +39,22 @@ public class TabletUI : MonoBehaviour {
         }
     }
 
+
     public void Resume()
     {
         Time.timeScale = 1f;
+        player.GetComponent<MouseLook>().enabled = true;
+        camera.GetComponent<MouseLook>().enabled = true;
+        player.GetComponent<PowerConsumer>().enabled = true;
+        player.GetComponent<PlayerCharacter>().enabled = true;
     }
 
     public void Pause()
     {
         Time.timeScale = 0f;
+        player.GetComponent<MouseLook>().enabled = false;
+        player.GetComponent<PlayerCharacter>().enabled = false;
+        player.GetComponent<PowerConsumer>().enabled = false;
+        camera.GetComponent<MouseLook>().enabled = false;
     }
 }

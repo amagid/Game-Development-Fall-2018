2dc3bab79cdee9595eb879b28c2f0b0b40184bd7
Aaron Magid <ahm64@case.edu>
2018-11-14 17:31:16 -0500

    feat(MaintenanceRoom): Added lights to the maintenance room.

---
+++ Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour {
+
+    public float onProbability = 0.9f;
+    public float flickerDelay = 0.1f;
+    public GameObject light;
+
+    private void Start()
+    {
+        this.flicker();
+    }
+    // Update is called once per frame
+    private void flicker() {
+        float val = UnityEngine.Random.value;
+		if (val < onProbability)
+        {
+            this.light.SetActive(true);
+        } else
+        {
+            this.light.SetActive(false);
+        }
+        Invoke("flicker", flickerDelay);
+	}
+}

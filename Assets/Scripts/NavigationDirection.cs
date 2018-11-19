69742d53a54307802cde48263bac50d89f38231a
TsueiCat <29084823+TsueiCat@users.noreply.github.com>
2018-11-19 15:29:23 -0500

    Revert "feat(Compass UI take 2)"

--- Assets/Scripts/NavigationDirection.cs
+++ Assets/Scripts/NavigationDirection.cs
@@ -7,25 +7,12 @@ using UnityEngine.UI;
 public class NavigationDirection : MonoBehaviour {
 	// stores the player object
 	[SerializeField] GameObject player;
-	[SerializeField] Image arrow;
-	[SerializeField] float arrowRotationSpeed;
-
-	private enum CardinalDirection{
+	// stores the text to display the direction to
+	[SerializeField] Text directionText;
+	
+	private enum CardinalDirection
+	{
 		North, South, East, West
-	}
-
-	private Vector3 target; 
-	private Dictionary<CardinalDirection, Vector3> directionToTarget = new Dictionary<CardinalDirection, Vector3>();
-
-	void Start(){
-		directionToTarget.Add (CardinalDirection.North, new Vector3 (0, 0, 0));
-		directionToTarget.Add (CardinalDirection.South, new Vector3 (0, 0, -180));
-		directionToTarget.Add (CardinalDirection.East, new Vector3 (0, 0, -90));
-		directionToTarget.Add (CardinalDirection.West, new Vector3 (0, 0, 90));
-
-		if (arrowRotationSpeed == 0) {
-			arrowRotationSpeed = 1.5f;
-		}
 	}
 
 	// Update is called once per frame
@@ -34,18 +21,13 @@ public class NavigationDirection : MonoBehaviour {
 		v.y = 0;
 		v.Normalize ();
 		if (Vector3.Angle (v, Vector3.forward) <= 45.0) {
-			RotateArrowTo(CardinalDirection.North);
+			directionText.text = CardinalDirection.North.ToString();
 		} else if (Vector3.Angle (v, Vector3.right) <= 45.0) {
-			RotateArrowTo(CardinalDirection.East);
+			directionText.text = CardinalDirection.East.ToString();
 		} else if (Vector3.Angle (v, Vector3.back) <= 45.0) {
-			RotateArrowTo(CardinalDirection.South);
+			directionText.text = CardinalDirection.South.ToString();
 		} else {
-			RotateArrowTo(CardinalDirection.West);
+			directionText.text = CardinalDirection.West.ToString();
 		}
-	}
-
-	private void RotateArrowTo(CardinalDirection cardinalDirection){
-		directionToTarget.TryGetValue(cardinalDirection, out target);
-		arrow.transform.rotation = Quaternion.Slerp(arrow.transform.rotation, Quaternion.Euler(target), Time.deltaTime * arrowRotationSpeed);
 	}
 }

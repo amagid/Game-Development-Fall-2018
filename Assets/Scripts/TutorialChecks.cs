80dee0d7f4345f2991cf06d89ef32be3316e6ae9
Rongjie Zeng <fatcha14@gmail.com>
2018-11-25 18:16:43 -0500

    <feat> Tutorial Dialogue
    <feat> Tutorial Dialogue

--- Assets/Scripts/TutorialChecks.cs
+++ Assets/Scripts/TutorialChecks.cs
@@ -10,11 +10,15 @@ public class TutorialChecks : MonoBehaviour {
 
     private bool hasReducedSanity = false;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]private GameObject previousDialogue;
+    [SerializeField]private GameObject currentDialogue;
+
+
+    // Use this for initialization
+    void Start () {
         this.player = GameObject.FindGameObjectWithTag("Player");
         this.player_character = this.player.GetComponent<PlayerCharacter>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -57,23 +61,33 @@ public class TutorialChecks : MonoBehaviour {
             {
                 case "CheckMovement":
                     //set current dialogue box to be ACTIVE
+                    currentDialogue.SetActive(true);
+                    previousDialogue.SetActive(true);
                     Debug.Log("Welcome Player! Use WASD to move around!");
                     break;
                 case "TutorialInstructions":
                     //set previous dialogue box to be INACTIVE
                     //set current dialogue box to be ACTIVE
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("This is the tutorial. " +
                         "Your sanity level is indicated on the left bar; power level on the right." +
                         "The bottom bar is a quick inventory menu." +
                         "Make sure you complete each objective in this tutorial before you proceed!");
                     break;
                 case "CheckPickup":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Batteries can be picked up using E when you are close. Pick up this battery!");
                     break;
                 case "CheckNoteReading":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("The purple tile in front of you is a Note. Press E to read a note when you are close.");
                     break;
                 case "CheckDoorOpening":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Doors are controlled by switches. Get close to the switch on your right. " +
                         "Press E to place a battery on the switch to activate the door.");
                     break;
@@ -83,29 +97,41 @@ public class TutorialChecks : MonoBehaviour {
                         player_character.reduceSanity(15f);
                         hasReducedSanity = true;
                     }
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("While in the dark, your sanity drops quite a bit. " +
                         "Press F to toggle your personal light to stop your sanity from decreasing." +
                         "Using the personal Light will consume your power.");
                     break;
                 case "CheckTurnOnLamp":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Light sources will bring your sanity back up." +
                         "Point the crosshair towards the lamp and hold LMB to power it until your sanity becomes full again.");
                     break;
                 case "CheckChargeFromFreezer":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Your power is low. " +
                         "You can siphon a small amount of power from the freezer on your right by getting close and holding RMB." +
                         "It will regenerate in some time.");
                     break;
                 case "CheckChargeFromBattery":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Pick up the battery in front of you." +
                         "Press I to open up the inventory UI." +
                         "Click on the battery and keep pressing the Charge button until it fills up your power. Press I again to close it.");
                     break;
                 case "CheckCrouch":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Most obstacles can be overcome by crouching and walking through it." +
                         "Press C to crouch and again to stand up when you are clear.");
                     break;
                 case "TutorialFinal":
+                    previousDialogue.SetActive(false);
+                    currentDialogue.SetActive(true);
                     Debug.Log("Congratz! You have finished the tutorial!" +
                         "You are now inside the facility. If you need help again, press Escape and go to the help menu. Good luck on your mission!");
                     break;

805bb63045417bc76ecfd0ded2783f5ba8174c9f
TungHo Lin(Dennis) <alexanderdennis@live.hk>
2018-11-25 00:44:29 -0500

    <feat>implemented check turning on lamp, charging from freezer and charging from battery

---
+++ Assets/Scripts/TutorialChecks.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialChecks : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            switch (this.name)
+            {
+                case "CheckMovement":
+                    Debug.Log("Checking Movement");
+                    break;
+                case "CheckPickup":
+                    Debug.Log("Checking Pickup");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

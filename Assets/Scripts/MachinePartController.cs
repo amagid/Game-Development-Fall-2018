2289ce7331bcf0d30ae1a5be604669b3f50d6701
David <dan57@case.edu>
2018-11-27 09:29:32 -0500

    scripts for the machine parts to work

---
+++ Assets/Scripts/MachinePartController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinePartController : MonoBehaviour {
+	public enum machinePartName {MachinePart1,MachinePart2};
+
+	[SerializeField] private int partNumber;
+
+	// deletes the object and adds it to the inventory
+	public void pickUpObject(Inventory inventory){
+		inventory.addItem(gameObject); 
+		gameObject.SetActive (false);
+	}
+
+	public int getPartNumber(){
+		return this.partNumber;
+	}
+}

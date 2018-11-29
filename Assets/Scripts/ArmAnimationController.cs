03bd2f31ffecb09ef0e93979fe9883462765b85e
Aaron Magid <ahm64@case.edu>
2018-11-29 10:10:56 -0500

    feat(ArmAnimation): Added an arm into the scene with an animation controller.

---
+++ Assets/Scripts/ArmAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAnimationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.J))
+        {
+            this.playAnimation();
+        }
+	}
+
+    public void playAnimation()
+    {
+        this.GetComponent<Animation>().Play();
+    }
+
+
+}

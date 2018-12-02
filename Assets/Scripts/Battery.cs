d5dd26e1231c2daed9210e76e5f6c8fe6a3f7a46
Aaron Magid <ahm64@case.edu>
2018-12-02 13:02:23 -0500

    added colliders to beams and deactivated rubble.

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -8,12 +8,31 @@ public class Battery : MonoBehaviour {
 	public int power_index = 100;
 	private PowerSource powerSource;
 	public PowerConsumer deviceBatteryIsAttachedTo = null; // WARNING: CURRENTLY ONLY SET UP FOR BATTERIES AND GENERIC SWITCH
+    private Light light1;
+    private Light light2;
 
 	// Use this for initialization
 	void Start () {
 		powerSource = new PowerSource (max_power, power_index);
 		powerSource.theBattery = this; // temporary bad code fix later
-	}
+        Transform light1obj = this.gameObject.transform.Find("light1");
+        Transform light2obj = this.gameObject.transform.Find("light2");
+        if (light1obj != null)
+        {
+            this.light1 = light1obj.GetComponent<Light>();
+        } else
+        {
+            this.light1 = null;
+        }
+        if (light2obj != null)
+        {
+            this.light2 = light2obj.GetComponent<Light>();
+        } else
+        {
+            this.light2 = null;
+        }
+
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -33,18 +52,22 @@ public class Battery : MonoBehaviour {
 		if(powerSource.isEmpty() && gameObject.GetComponent<Renderer>().material.color != Color.red)
 		{
 			this.gameObject.GetComponent<Renderer>().material.color = Color.red;
-			this.gameObject.transform.Find("light1").GetComponent<Light>().color = Color.red;
-			this.gameObject.transform.Find("light2").GetComponent<Light>().color = Color.red;
-			/*if (this.switchController != null) NOTE: Was in the previous doc will probably delete
+            if (this.light1 != null)
+                this.light1.color = Color.red;
+            if (this.light2 != null)
+                this.light2.color = Color.red;
+            /*if (this.switchController != null) NOTE: Was in the previous doc will probably delete
 			{
 				this.switchController.deactivate();
 			}*/
-		}
+        }
 		else if (!powerSource.isEmpty() && gameObject.GetComponent<Renderer>().material.color != Color.green)
 		{
 			this.gameObject.GetComponent<Renderer>().material.color = Color.green;
-			this.gameObject.transform.Find("light1").GetComponent<Light>().color = Color.green;
-			this.gameObject.transform.Find("light2").GetComponent<Light>().color = Color.green;
+            if (this.light1 != null)
+    			this.light1.color = Color.green;
+            if (this.light2 != null)
+                this.light2.color = Color.green;
 		}
 	}
 
